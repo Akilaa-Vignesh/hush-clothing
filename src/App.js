e2c7567b42e40c9f460c4d7e03c09d6320e75626
Akilaa Vignesh <akilaa.vignesh@gds.ey.com>
2021-02-16 14:53:15 +0530

    created a header and addedd navigation to the options and a logo

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import logo from "./logo.svg";
 import "./App.css";
-import { HomePage } from "./pages/homepage/homepage.component";
 import { Route, Switch } from "react-router-dom";
+import { HomePage } from "./pages/homepage/homepage.component";
 import ShopPage from "./pages/shoppage/shop.component";
+import Header from "./components/header/header.component";
 
 function App() {
   return (
     <div className="App">
+      <Header></Header>
       <Switch>
         <Route exact path="/" component={HomePage}></Route>
         <Route exact path="/shop" component={ShopPage}></Route>
